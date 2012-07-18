260dcd3c25743b34cbef8c120e160fe9c0e3017e
roger <igor.onyshchenko@gmail.com>
2012-07-18 18:05:02 +0300

    i

--- git-backup.py
+++ git-backup.py
@@ -6,9 +6,19 @@ now = datetime.datetime.now().strftime("%Y-%m-%d")
 backupBasedir = "/mnt/backup/git/"
 gitBasedir = "/home/git/"
 
-# ukrainianside
-usTar = backupBasedir + "ukrainianside/" + now + ".tar"
 
-subprocess.call("tar cvf " + usTar + " " + gitBasedir + "ukrainianside/*", shell=True)
-subprocess.call("gzip -9 " + usTar, shell=True)
+tarproject("auth")
+tarproject("crontab")
+tarproject("hikesrilanka")
+tarproject("hyperload")
+tarproject("hyqd")
+tarproject("hytcpd")
+tarproject("picpuk")
+tarproject("ukrainianside")
 
+
+def tarproject(pName):
+    pTar = backupBasedir + pName + "/" + now + ".tar"
+    
+    subprocess.call("tar cvf " + pTar + " " + gitBasedir + pName + "/*", shell=True)
+    subprocess.call("gzip -9 " + pTar, shell=True)
