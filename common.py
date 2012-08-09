959a40cd2ccff3137739673e74e660d77670ca6d
roger <igor.onyshchenko@gmail.com>
2012-08-09 17:03:55 +0300

    i

--- common.py
+++ common.py
@@ -5,3 +5,7 @@ now = datetime.datetime.now().strftime("%Y-%m-%d")
 backupBasedir = "/mnt/backup/"
 backupContentDir = backupBasedir + "content/"
 backupDatabaseDir = backupBasedir + "database/"
+backupMailDir = backupBasedir + 'mail'
+
+mailerUid = 5003
+mailerGid = 5003
--- projects/postfix.py
+++ projects/postfix.py
@@ -5,3 +5,13 @@ import projects.base
 class Postfix(projects.base.Base):
     def __init__(self):
         self.pName = "postfix"
+        self.mailBasedir = '/home'
+        
+    def tarmail(self):
+        os.chdir(self.mailBasedir)
+        
+        pTar = common.backupMailDir + "/" + common.now + ".tar.gz"
+    
+        tar = tarfile.open(pTar, "w:gz")
+        tar.add("mailer")
+        tar.close()
